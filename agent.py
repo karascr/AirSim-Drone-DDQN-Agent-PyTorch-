11b52b2a55c0b0c329003c599d48797af7799a7b
Muhammet Kara <muhammetkaratr@gmail.com>
2021-03-02 22:02:16 +0300

    New files uploaded

---
+++ agent.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+"""Environment for Microsoft AirSim Unity Quadrotor
+
+- Author: Subin Yang
+- Contact: subinlab.yang@gmail.com
+"""
+import math
+import random
+from collections import deque
+import airsim
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from PIL import Image
+from setuptools import glob
+
+from env import DroneEnv
+
+#device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+env = DroneEnv()
+
+params = {
+        "max_episodes": 10000,
+        "save_interval": 50,
+        "eps_start":0.9,
+        "eps_end":0.05,
+        "eps_decay":200,
+        "gamma":0.8,
+        "learning_rate":0.001,
+        "batch_size":1
+        }
+
+
+class DQN(nn.Module):
+    def __init__(self, in_channels=1, num_actions=7):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 84, kernel_size=4, stride=4)
+        self.conv2 = nn.Conv2d(84, 42, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(42, 21, kernel_size=2, stride=2)
+        self.fc4 = nn.Linear(21*4*4, 168)
+        self.fc5 = nn.Linear(168, num_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc4(x))
+        return self.fc5(x)
+
+
+class Agent:
+    def __init__(self):
+        self.eps_start = params["eps_start"]
+        self.eps_end = params["eps_end"]
+        self.eps_decay = params["eps_decay"]
+        self.gamma = params["gamma"]
+        self.learning_rate = params["learning_rate"]
+        self.batch_size = params["batch_size"]
+        self.max_episodes = params["max_episodes"]
+        self.save_interval = params["save_interval"]
+
+        self.dqn = DQN()
+
+        cwd = os.getcwd()
+        model_dir = os.path.join(cwd, "saved models")
+
+        if not os.path.exists(model_dir):
+            os.mkdir("saved models")
+
+
+        files = glob.glob(model_dir + '\\*.pth')
+        if len(files) > 0:
+            file = files[-1]
+            checkpoint = torch.load(file)
+
+            self.dqn = self.dqn.load_state_dict(checkpoint)
+            print("checkpoint loaded: ", file)
+
+        #self.dqn = self.dqn.to(device) # to use GPU
+
+        """Get observation"""
+        responses = env.client.simGetImages(
+            [airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)]
+        )
+
+        """Transform input binary array to image"""
+        response = responses[0]
+        img1d = np.fromstring(
+            response.image_data_uint8, dtype=np.uint8
+        )
+        img_rgba = img1d.reshape(
+            response.height, response.width, 3
+        )
+        image = Image.fromarray(img_rgba)
+        im_final = np.array(image.resize((84, 84)).convert("L"))
+
+        """oimg = image = Image.fromarray(im_final)
+        oimg.save("a.jpg")"""
+
+        #tensor = torch.from_numpy(im_final)
+        tensor = self.transformToTensor(im_final)
+
+        self.model = self.dqn.forward(tensor)
+        self.memory = deque(maxlen=10000)
+        self.optimizer = optim.Adam(self.dqn.parameters(), self.learning_rate)
+        self.steps_done = 0
+
+    def transformToTensor(self, img):
+        tensor = torch.Tensor(img)
+        tensor = tensor.unsqueeze(0)
+        tensor = tensor.unsqueeze(0)
+        tensor = tensor.float()
+        return tensor
+
+    def act(self, state):
+        eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * math.exp(
+            -1.0 * self.steps_done / self.eps_decay
+        )
+        self.steps_done += 1
+        if random.random() > eps_threshold:
+            data = self.dqn(state).data
+            action = np.argmax(data.squeeze().numpy())
+
+            return torch.LongTensor([action])
+        else:
+            action = [random.randrange(0, 7)]
+            return torch.LongTensor([action])
+
+    def memorize(self, state, action, reward, next_state):
+        self.memory.append(
+            (
+                state,
+                action,
+                torch.FloatTensor([reward]),
+                self.transformToTensor(next_state),
+            )
+        )
+
+    def learn(self):
+        if len(self.memory) < self.batch_size:
+            return
+
+        print("learning started")
+
+        batch = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states = zip(*batch)
+
+        states = torch.cat(states)
+        actions = torch.cat(actions)
+        rewards = torch.cat(rewards)
+        next_states = torch.cat(next_states)
+
+        current_q = self.dqn(states)
+        max_next_q = self.dqn(next_states).detach().max(1)[0]
+        expected_q = rewards + (self.gamma * max_next_q)
+
+        """print(current_q)
+        print("current:", current_q.shape)
+        print("current s:", current_q.squeeze().shape)
+        print("expected:", expected_q.shape)"""
+
+        loss = F.mse_loss(current_q.squeeze(), expected_q)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def train(self):
+
+        score_history = []
+        reward_history = []
+        score = 0
+        episode = 1
+
+        for e in range(1, self.max_episodes + 1):
+            state = env.reset()
+            steps = 0
+            while True:
+                #state = torch.FloatTensor([state])
+                state = self.transformToTensor(state)
+
+                action = self.act(state)
+                next_state, reward, done = env.step(action)
+
+                self.memorize(state, action, reward, next_state)
+                self.learn()
+
+                state = next_state
+                steps += 1
+                score += reward
+                if done:
+                    print("episode:{0}, reward: {1}, score: {2}".format(e, reward, score))
+                    print("----------------------------------------------------")
+                    score_history.append(steps)
+                    reward_history.append(reward)
+                    f = open("reward.txt", "a")
+                    f.write(str(reward))
+                    f.close()
+                    f2 = open("score.txt", "a")
+                    f2.write(str(score))
+                    f2.close()
+                    break
+
+            if episode % params["save_interval"] == 0:  # "epoch": epoch + 1,x
+                #torch.save({'params': params}, model_dir + '//model_EPISODES_DQN_DRONE{}.pth'.format(episode))
+                torch.save(self.dqn.state_dict(), model_dir + '//model_EPISODES_DQN_DRONE{}.pth'.format(episode))
+            episode += 1
---
+++ env.py
@@ -0,0 +1,187 @@
+#! /usr/bin/env python
+"""Environment for Microsoft AirSim Unity Quadrotor using AirSim python API
+
+- Author: Subin Yang
+- Contact: subinlab.yang@gmail.com
+- Date: 2019.06.20.
+"""
+import csv
+import math
+import pprint
+import time
+
+import torch
+from PIL import Image
+
+import numpy as np
+
+import airsim
+#import setup_path
+
+MOVEMENT_INTERVAL = 1
+
+
+class DroneEnv(object):
+    """Drone environment class using AirSim python API"""
+
+    def __init__(self):
+        self.client = airsim.MultirotorClient()
+
+        self.state = {
+            "position": self.client.getMultirotorState().kinematics_estimated.position,
+            "prev_position": self.client.getMultirotorState().kinematics_estimated.position
+        }
+
+        self.best_dist = 1000000
+
+        self.state["prev_position"] = self.state["position"]
+        collision = False
+
+        self.quad_offset = (0, 0, 0)
+
+
+        self.ep = 0
+
+    def step(self, action):
+        """Step"""
+
+        print("doing step")
+
+        self.quad_offset = self.interpret_action(action)
+        print("quad_offset: ", self.quad_offset)
+
+        quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+        self.client.moveByVelocityAsync(
+            quad_vel.x_val + self.quad_offset[0],
+            quad_vel.y_val + self.quad_offset[1],
+            quad_vel.z_val + self.quad_offset[2],
+            MOVEMENT_INTERVAL
+        ).join()
+        time.sleep(0.5)
+
+        quad_state = self.client.getMultirotorState().kinematics_estimated.position
+        quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
+
+        self.state["prev_position"] = self.state["position"]
+        self.state["position"] = self.client.getMultirotorState().kinematics_estimated.position
+
+        collision = self.client.simGetCollisionInfo().has_collided
+
+        result = self.compute_reward(quad_state, quad_vel, collision)
+        state = self.get_obs()
+        done = self.isDone(result)
+        return state, result, done
+
+    def reset(self):
+        self.client.reset()
+        self.best_dist = self.get_distance(self.state["position"])
+        self.client.enableApiControl(True)
+        self.client.armDisarm(True)
+
+        self.state["prev_position"] = self.state["position"]
+        collision = False
+
+        print("takeoff")
+        self.client.takeoffAsync().join()
+
+        obs = self.get_obs()
+
+        return obs
+
+    def get_obs(self):
+        """Get observation"""
+        responses = self.client.simGetImages(
+            [airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)]
+        )
+        obs = self.transform_input(responses)
+        return obs
+
+    def get_distance(self, quad_state):
+        """Get distance between current state and goal state"""
+        pts = np.array([3, -76, -7])
+        quad_pt = np.array(list((quad_state.x_val, quad_state.y_val, quad_state.z_val)))
+        dist = np.linalg.norm(quad_pt - pts)
+        return dist
+
+    def compute_reward(self, quad_state, quad_vel, collision):
+        """Compute reward"""
+
+        reward = -1
+
+        if collision:
+            reward = -100
+        elif self.state["position"].z_val < -23:
+            reward = -100
+        else:
+            dist = self.get_distance(quad_state)
+
+            print("dist: ", dist, " best_dist: ", self.best_dist)
+
+            diff = dist - self.best_dist
+
+            if diff < -1:
+                reward = 2
+            elif dist < 10:
+                reward = 500
+
+            if dist < self.best_dist:
+                self.best_dist = dist
+
+        print("reward: ", reward)
+
+        return reward
+
+
+    def isDone(self, reward):
+        """Check if episode is done"""
+        done = 0
+        if reward <= -10:
+            done = 1
+            self.reset()
+            time.sleep(1)
+        elif reward > 499:
+            done = 1
+            self.reset()
+            time.sleep(1)
+        return done
+
+    def transform_input(self, responses):
+        """Transform input binary array to image"""
+        response = responses[0]
+        img1d = np.fromstring(
+            response.image_data_uint8, dtype=np.uint8
+        )
+        img_rgba = img1d.reshape(
+            response.height, response.width, 3
+        )
+        image = Image.fromarray(img_rgba)
+        im_final = np.array(image.resize((84, 84)).convert("L"))
+
+        return im_final
+
+    def transformToTensor(self, img):
+        tensor = torch.Tensor(img)
+        tensor = tensor.unsqueeze(0)
+        tensor = tensor.unsqueeze(0)
+        tensor = tensor.float()
+        return tensor
+
+    def interpret_action(self, action):
+        """Interprete action"""
+        scaling_factor = 5
+        if action.item() == 0:
+            self.quad_offset = (0, 0, 0)
+        elif action.item() == 1:
+            self.quad_offset = (scaling_factor, 0, 0)
+        elif action.item() == 2:
+            self.quad_offset = (0, scaling_factor, 0)
+        elif action.item() == 3:
+            self.quad_offset = (0, 0, scaling_factor)
+        elif action.item() == 4:
+            self.quad_offset = (-scaling_factor, 0, 0)
+        elif action.item() == 5:
+            self.quad_offset = (0, -scaling_factor, 0)
+        elif action.item() == 6:
+            self.quad_offset = (0, 0, -scaling_factor)
+
+        return self.quad_offset
